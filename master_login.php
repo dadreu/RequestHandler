440c2b7d0dd57478c9659c345c701bfb9e625fa3
Natasha1107 <natalyamozhaeva1107@gmail.com>
2025-04-02 04:04:40 +0500

    Дополнение нормализации номера в серверной части

--- master_login.php
+++ master_login.php
@@ -3,8 +3,19 @@ include 'config.php'; // Подключение к базе данных
 header('Content-Type: application/json');
 $response = ['success' => false];
 
-if (isset($_POST['phone']) && isset($_POST['password'])) {
-    $phone = $_POST['phone'];
+// Функция нормализации номера телефона
+function normalizePhone($phone) {
+    $phone = preg_replace('/[^0-9]/', '', $phone); // Удаляем все нечисловые символы
+    if (strlen($phone) == 11 && $phone[0] == '8') {
+        $phone = '7' . substr($phone, 1); // Заменяем 8 на 7
+    }
+    return $phone;
+}
+
+if (!isset($_POST['phone']) || !isset($_POST['password'])) {
+    $response['message'] = 'Не переданы номер телефона или пароль';
+} else {
+    $phone = normalizePhone($_POST['phone']); // Нормализуем номер телефона
     $password = $_POST['password'];
 
     // Подготовленный запрос к таблице Masters
@@ -12,12 +23,16 @@ if (isset($_POST['phone']) && isset($_POST['password'])) {
     $stmt->execute(['phone' => $phone]);
     $master = $stmt->fetch(PDO::FETCH_ASSOC);
 
+    // Проверяем, найден ли пользователь
+    if (!$master) {
+        $response['message'] = 'Номер телефона не зарегистрирован';
+    }
     // Сравнение пароля напрямую
-    if ($master && $master['password'] === $password) {
+    elseif ($master['password'] === $password) {
         $response['success'] = true;
         $response['master_id'] = $master['id'];
     } else {
-        $response['message'] = 'Неверный номер телефона или пароль';
+        $response['message'] = 'Неверный пароль';
     }
 }
 
